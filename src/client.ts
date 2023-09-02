870371798876b55c8c5ab64c8212a34bafcabc3f
devjiwonchoi <devjiwonchoi@gmail.com>
2023-09-03 03:39:46 +0900

    feat: add client

---
+++ src/client.ts
@@ -0,0 +1,13 @@
+import { Account } from './account'
+import { LichessHeaders } from './types'
+
+export class Client {
+  private headers: LichessHeaders
+  public account: Account
+  constructor(readonly token: string) {
+    this.headers = {
+      Authorization: `Bearer ${this.token}`,
+    }
+    this.account = new Account(this.headers)
+  }
+}
---
+++ src/constants.ts
@@ -0,0 +1,1 @@
+export const LICHESS_API_URL = 'https://lichess.org/api'
