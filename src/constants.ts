4619ef789e319126d9857c7d4de8283404fb4aec
devjiwonchoi <devjiwonchoi@gmail.com>
2023-09-04 23:48:08 +0900

    chore: dotenv import to cli

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,2 @@
-import 'dotenv/config'
-
 export const LICHESS_API_URL =
   process.env.LICHESS_DEV_API_URL ?? 'https://lichess.org/api'
--- test/test-utils.ts
+++ test/test-utils.ts
@@ -1,4 +1,3 @@
-import 'dotenv/config'
 import { Client } from '../src/client'
 
 export const client = new Client(process.env.LICHESS_DEV_TOKEN!)
