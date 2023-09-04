55431c25191842ab7699ef966074e30567d43d14
devjiwonchoi <devjiwonchoi@gmail.com>
2023-09-05 00:58:39 +0900

    chore: add devjiwonchoi_equine for github actions

--- test/challenge.test.ts
+++ test/challenge.test.ts
@@ -8,10 +8,11 @@ describe('Challenge', () => {
     expect(data).toHaveProperty('out')
   })
   it('should create and cancel a challenge', async () => {
-    const botName = process.env.NODE_ENV === 'test' ? 'Bot1' : 'LeelaChess'
+    const botName =
+      process.env.NODE_ENV === 'test' ? 'Bot1' : 'devjiwonchoi_equine'
     const res = await client.challenge.create(botName)
     const data = await res.json()
-    const res2 = await client.challenge.cancel(data.challenge?.id)
+    const res2 = await client.challenge.cancel(data.challenge.id)
     const data2 = await res2.json()
     expect(data.challenge).toHaveProperty('id')
     expect(data2.ok).toBe(true)
