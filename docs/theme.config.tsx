f69d03c45587f139ee45a24b8864fc63ed3e0f6a
devjiwonchoi <devjiwonchoi@gmail.com>
2023-09-15 13:50:37 +0900

    docs: init nextra

---
+++ docs/theme.config.tsx
@@ -0,0 +1,10 @@
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Equine 🎠</span>,
+  project: {
+    link: 'https://github.com/devjiwonchoi/equine',
+  },
+}
+
+export default config
