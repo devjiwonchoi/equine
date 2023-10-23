e02bdf148e205ef767e0d83b8cfb038ba5434d45
devjiwonchoi <devjiwonchoi@gmail.com>
2023-10-24 05:33:32 +0900

    ready for incremental migration

--- src/client.ts
+++ src/client.ts
@@ -8,30 +8,39 @@ import { fetcher } from './utils'
 import { LichessHeaders } from './types'
 
 export class Equine {
-  public account: Account
   private fetcher: Function
-  // public analysis: Analysis
-  // public board: Board
-  // public bot: Bot
-  // public challenge: Challenge
-  // public message: Message
-  // public simuls: Simuls
-  // public tv: TV
-  // public user: User
-  // public users: Users
+  // TODO: remove this and integrate fetcher
+  private readonly headers: LichessHeaders
+
+  public account: Account
+  public analysis: Analysis
+  public board: Board
+  public bot: Bot
+  public challenge: Challenge
+  public message: Message
+  public simuls: Simuls
+  public tv: TV
+  public user: User
+  public users: Users
 
   constructor(private readonly token: string) {
     this.fetcher = (endpoint: string, post?: boolean) =>
       fetcher({ endpoint, token: this.token, post })
+
+    // TODO: remove this and integrate fetcher
+    this.headers = {
+      Authorization: `Bearer ${this.token}`,
+    }
+
     this.account = new Account(this.fetcher)
-    // this.analysis = new Analysis(this.token)
-    // this.board = new Board(this.token)
-    // this.bot = new Bot(this.token)
-    // this.challenge = new Challenge(this.token)
-    // this.message = new Message(this.token)
-    // this.simuls = new Simuls(this.token)
-    // this.tv = new TV(this.token)
-    // this.user = new User(this.token)
-    // this.users = new Users(this.token)
+    this.analysis = new Analysis(this.headers)
+    this.board = new Board(this.headers)
+    this.bot = new Bot(this.headers)
+    this.challenge = new Challenge(this.headers)
+    this.message = new Message(this.headers)
+    this.simuls = new Simuls(this.headers)
+    this.tv = new TV(this.headers)
+    this.user = new User(this.headers)
+    this.users = new Users(this.headers)
   }
 }
