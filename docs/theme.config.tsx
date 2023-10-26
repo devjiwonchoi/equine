af3cf4615516c125257e5b0fc97d4e7b88351493
devjiwonchoi <devjiwonchoi@gmail.com>
2023-10-27 02:17:53 +0900

    modify logo

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -1,7 +1,26 @@
+import Image from 'next/image'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Equine 🎠</span>,
+  logo: (
+    <Image
+      src="/equine.svg"
+      alt="Equine Logo"
+      width="48"
+      height="48"
+      style={{ filter: 'invert(1)' }}
+    />
+  ),
+  head: (
+    <>
+      <link
+        rel="icon"
+        type="image/x-icon"
+        href="/equine.svg"
+        style={{ filter: 'revert' }}
+      />
+    </>
+  ),
   project: {
     link: 'https://github.com/devjiwonchoi/equine',
   },
