48879ab881371630bf7aed5308fe52abe2c13421
devjiwonchoi <devjiwonchoi@gmail.com>
2023-12-15 18:14:56 +0900

    Refactor LICHESS_API_URL in constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,3 @@
-const LICHESS_TEST_API_URL =
-  process.env.NODE_ENV === 'test' && 'https://lichess.dev/api'
-
-export const LICHESS_API_URL = LICHESS_TEST_API_URL || 'https://lichess.org/api'
+export const LICHESS_API_URL = `https://lichess.${
+  process.env.NODE_ENV === 'test' ? 'dev' : 'org'
+}/api`
