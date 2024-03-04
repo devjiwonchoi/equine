79040fbc30dbd8498df591424d5f833d19abc8c7
nicholasdejong <nickacidegamer@gmail.com>
2024-03-04 17:09:34 +0200

    Add `JSONStream`, remove JSON parsing

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,30 @@
 import { LICHESS_API_URL } from './constants'
 
+/// Processes a text-decoded stream and tries to parse it into JSON.
+export class JSONStream extends TransformStream {
+    constructor() {
+        super({
+            start() {},
+            flush() {},
+            async transform(chunk: string, controller: TransformStreamDefaultController) {
+                if (chunk.trim() === '') {
+                    controller.enqueue({})
+                } else {
+                    let parsed
+                    try {
+                        parsed = JSON.parse(chunk)
+                    } catch (e) {
+                        controller.error(`Error parsing JSON: ${e}`)
+                        parsed = chunk
+                    }
+
+                    controller.enqueue(parsed)
+                }
+            }
+        })
+    }
+}
+
 export async function fetcher({
   endpoint,
   token,
@@ -20,9 +45,8 @@ export async function fetcher({
       body,
     }
     const response = await fetch(`${LICHESS_API_URL}${endpoint}`, fetchOptions)
-    const data = await response.json()
 
-    return data
+    return response
   } catch (error) {
     throw error
   }
