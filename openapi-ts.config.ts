a32cd987220cbd1b489ef6e1974bb64330b9ffd8
Jiwon Choi <devjiwonchoi@gmail.com>
2025-04-05 23:41:56 +0200

    ready for 1.0.0-alpha.0 (#128)

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from '@hey-api/openapi-ts'
 
 export default defineConfig({
+  // TODO: Lichess OpenAPI endpoint
   input: './src/openapi.json',
   output: './src/client/',
   plugins: ['@hey-api/client-fetch'],
